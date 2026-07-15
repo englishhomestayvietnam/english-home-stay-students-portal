68111926f91314abc3d457018d5809b61fc88cad
Sai Htet Myat Naing <saihtetmyatnaing4@gmail.com>
2026-07-15 21:11:06 +0700

    Fix Payload CMS not-found.tsx TypeScript error

--- app/(payload)/admin/[[...segments]]/not-found.tsx
+++ app/(payload)/admin/[[...segments]]/not-found.tsx
@@ -2,6 +2,20 @@ import config from '@/payload.config'
 import { NotFoundPage } from '@payloadcms/next/views'
 import { importMap } from '../importMap.js'
 
-export default function NotFound() {
-  return NotFoundPage({ config, importMap })
+type Args = {
+  params: Promise<{
+    segments: string[]
+  }>
+  searchParams: Promise<{
+    [key: string]: string | string[]
+  }>
+}
+
+export default function NotFound(args: Args) {
+  return NotFoundPage({ 
+    config, 
+    importMap, 
+    params: args?.params || Promise.resolve({ segments: [] }), 
+    searchParams: args?.searchParams || Promise.resolve({}) 
+  })
 }
